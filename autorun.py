6a25fe2eafd5ae887c4ae0d51e74f29b6ef65180
B3nac <Rockapparatus@yahoo.com>
2015-03-10 20:03:07 -0700

    Fixed autorun not looping

--- autorun.py
+++ autorun.py
@@ -1,21 +1,21 @@
-#Autorun script created by Kyle Benac
-
 import B3SI
 from time import sleep
 
 def main():
-    print(" ------------------------------------------- ")
-    print(" |           Starting autopilot!           | ")
-    print(" ------------------------------------------- ")
-    auto = B3SI.B3SI()
-    auto.check_temp()
-    auto.a_temp()
-    auto.take_pic()
-    auto.sort_uniq()
-    print(" -------------------------------------------------- ")
-    print(" | Waiting a couple minutes before another loop.| ")
-    print(" -------------------------------------------------- ")
-    sleep(200)
-
-if __name__ == '__main__':
-    main()
+    done = False
+    while not done:
+        print(" ------------------------------------------- ")
+        print(" |           Starting autopilot!           | ")
+        print(" ------------------------------------------- ")
+        auto = B3SI.B3()
+        auto.check_temp()
+        auto.a_temp()
+        auto.take_pic()
+        auto.sort_uniq()
+        print(" -------------------------------------------------- ")
+        print(" | Waiting a couple minutes before another loop.| ")
+        print(" -------------------------------------------------- ")
+        sleep(200)
+    
+if __name__ == '__main__': 
+        main()

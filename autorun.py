a26aa3ee8132f3f48cd0bf0f66dc935083a59e64
B3nac <Rockapparatus@yahoo.com>
2015-08-23 11:44:23 -0700

    Revert

--- autorun.py
+++ autorun.py
@@ -1,20 +1,21 @@
-import B3
+import B3SI
 from time import sleep
 
 def main():
-    print(" ------------------------------------------- ")    
-    print(" |           Starting autopilot!           | ")
-    print(" ------------------------------------------- ")
-    auto = B3.B3interface()
+    done = False
+    while not done:
+        print(" ------------------------------------------- ")
+        print(" |           Starting autopilot!           | ")
+        print(" ------------------------------------------- ")
+        auto = B3SI.B3()
+        auto.check_temp()
+        auto.a_temp()
+        auto.take_pic()
+        auto.sort_uniq()
+        print(" -------------------------------------------------- ")
+        print(" | Waiting a couple minutes before another loop.| ")
+        print(" -------------------------------------------------- ")
+        sleep(200)
     
-    auto.check_temp()
-    auto.a_temp() 
-    auto.sort_uniq()
-    print(" -------------------------------------------------- ") 
-    print(" | Waiting a couple minutes before another rescan.| ")
-    print(" -------------------------------------------------- ")
-    sleep(200)
-    main()
-
-if __name__ == '__main__':
-    main()
+if __name__ == '__main__': 
+        main()

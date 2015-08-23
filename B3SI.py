3ac5c76458bc1ec7d4cb7de61d5f9bf340f13e6f
B3nac <Rockapparatus@yahoo.com>
2015-08-23 11:41:14 -0700

    Got rid of un-needed lines.

--- B3SI.py
+++ B3SI.py
@@ -145,4 +145,4 @@ def main():
 
 if __name__ == '__main__':
 	main()
-import os
+
--- autorun.py
+++ autorun.py
@@ -1,21 +1,20 @@
-#Autorun script created by Kyle Benac
-import B3SI
+import B3
 from time import sleep
 
 def main():
-    done = False
-    while not done:
-        print(" ------------------------------------------- ")
-        print(" |           Starting autopilot!           | ")
-        print(" ------------------------------------------- ")
-        auto = B3SI.B3()
-        auto.check_temp()
-        auto.a_temp()
-        auto.take_pic()
-        print(" -------------------------------------------------- ")
-        print(" | Waiting a couple minutes before another loop.  | ")
-        print(" -------------------------------------------------- ")
-        sleep(200)
+    print(" ------------------------------------------- ")    
+    print(" |           Starting autopilot!           | ")
+    print(" ------------------------------------------- ")
+    auto = B3.B3interface()
     
-if __name__ == '__main__': 
-        main()
+    auto.check_temp()
+    auto.a_temp() 
+    auto.sort_uniq()
+    print(" -------------------------------------------------- ") 
+    print(" | Waiting a couple minutes before another rescan.| ")
+    print(" -------------------------------------------------- ")
+    sleep(200)
+    main()
+
+if __name__ == '__main__':
+    main()
